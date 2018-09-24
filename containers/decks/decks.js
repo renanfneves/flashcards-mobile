0916443531f76424e7d35f977740a2462e030fd3
Renan Floriano Neves <renan.neves@M004470.local>
2018-09-24 15:09:00 -0300

    feat(containers): Add containers to project

--- containers/decks/decks.js
+++ containers/decks/decks.js
@@ -3,60 +3,23 @@ import { connect } from 'react-redux';
 import { ListItem } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import { View, FlatList, AsyncStorage } from 'react-native';
-import { getDecks } from '../../actions/decks';
+import { getDecks, INITIAL_DECKS, AVAILABLE_DECKS } from '../../actions/decks';
 import { list, container, Separator } from "./decks.styles";
 
-const AVAILABLE_DECKS = "DECKS";
-
-const infoDeckState = {
-  React: {
-    title: 'React',
-    questions: [
-      {
-        question: 'React is cool?',
-        answer: 'Yes'
-      },
-      {
-        question: 'React is a framework?',
-        answer: 'Yes'
-      }
-    ]
-  },
-  JavaScript: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'JavaScript is cool?',
-        answer: 'Yes'
-      }
-    ]
-  },
-  Ruby: {
-    title: 'Ruby',
-    questions: [
-      {
-        question: 'Ruby is cool?',
-        answer: 'Yes.'
-      }
-    ]
-  },
-}
-
 class Decks extends Component {
-  state = {
-    metaInfo: []
+  static fetchData(dispatch) {
+    return dispatch(getDecks());
   }
 
   componentDidMount() {
-    AsyncStorage.getItem(AVAILABLE_DECKS, storaged => {
-      if(storaged && storaged.length) {
-        return this.setState({ metaInfo: storaged });
-      }
-
-      AsyncStorage.setItem(AVAILABLE_DECKS, JSON.stringify(infoDeckState), () => {
-        return this.setState({ metaInfo: infoDeckState });
-
-      });
+		const { dispatch } = this.props;
+		// Decks.fetchData(this.props.dispatch);
+		
+		AsyncStorage.getItem(AVAILABLE_DECKS, decks => {
+      if(decks && decks.length) { return dispatch(getDecks(decks)); }
+  
+			AsyncStorage.setItem(AVAILABLE_DECKS, JSON.stringify(INITIAL_DECKS), 
+			() => dispatch(getDecks(INITIAL_DECKS)));
     });
   }
 
@@ -75,9 +38,9 @@ class Decks extends Component {
 	);
 
 	render() {
-		const { metaInfo } = this.state;
-		const data = metaInfo ? Object.keys(metaInfo).map((key) => {
-			const details = metaInfo[key]
+		const { availableDecks } = this.props;
+		const data = availableDecks ? Object.keys(availableDecks).map((key) => {
+			const details = availableDecks[key]
 			return { key: key, total: details.questions ? details.questions.length : 0 }
     }) : []
 
